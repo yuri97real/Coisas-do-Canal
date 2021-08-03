3762eb59068bbef90437dce282e840368c9e8551
Yuri Seabra Maciel <yuri97@pop-os.localdomain>
2021-08-03 18:13:37 -0400

    Segunda Parte da Máscara Direita-Esquerda

--- Máscara Direita-Esquerda/mask.js
+++ Máscara Direita-Esquerda/mask.js
@@ -2,19 +2,44 @@ const inputMask = document.querySelector("#mask")
 
 const isANumber = (key) => !isNaN(key) && key != "" && key != " "
 
+const patternData = (isDeleting, key = "") => {
+
+    const onlyNumbers = inputMask.value.replace(".", "").split("")
+
+    if(isDeleting) {
+        onlyNumbers.pop()
+        onlyNumbers.splice(0, 0, 0)
+    } else {
+        onlyNumbers.shift()
+        onlyNumbers.push(key)
+    }
+
+    onlyNumbers.splice(2, 0, ".")
+
+    inputMask.value = onlyNumbers.join("")
+
+}
+
+inputMask.addEventListener("paste", e => e.preventDefault())
+
+inputMask.addEventListener("keydown", e => {
+
+    const deleteKeys = ["Backspace", "Delete"]
+    
+    if( !deleteKeys.includes(e.key) ) return
+
+    e.preventDefault()
+
+    patternData(true)
+
+})
+
 inputMask.addEventListener("keypress", e => {
 
     e.preventDefault()
 
     if( !isANumber(e.key) ) return
 
-    const onlyNumbers = inputMask.value.replace(".", "").split("")
-
-    onlyNumbers.shift()
-    onlyNumbers.push(e.key)
-
-    onlyNumbers.splice(1, 0, ".")
-
-    inputMask.value = onlyNumbers.join("")
+    patternData(false, e.key)
 
 })
